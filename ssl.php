f545fc5b2bfbb233518e5d2b9694f346aa4320eb
Saad Mirza <saadmirza009@gmail.com>
2017-09-23 00:02:23 -0700

    Removing Too Many Redirects

--- ssl.php
+++ ssl.php
@@ -8,11 +8,7 @@ function redirect_ssl() {
         // redirecting to ssl.
         $CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
         if ($_SERVER['SERVER_PORT'] != 443) redirect($CI->uri->uri_string());
-    } else {
-        // redirecting with no ssl.
-        $CI->config->config['base_url'] = str_replace('https://', 'http://', $CI->config->config['base_url']);
-        if ($_SERVER['SERVER_PORT'] == 443) redirect($CI->uri->uri_string());
-    }
+    } 
 }
 
  ?>
